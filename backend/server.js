a44998429e0f35c01a9f620584ff626835663abc
Christophe Romand <ackheron@tuta.io>
2022-01-07 13:03:43 +0100

    Import module dotenv

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,22 @@
 
 const http = require("http");
 
+// Importation du module dotenv pour utiliser les variables d'environnement écrites dans le  fichier .env dans le répertoire racine du dossier backend
+
+const dotenv = require("dotenv");
+
 // Appel du fichier app.js
 
 const app = require("./app");
 
 /************************** LE SERVEUR *******************************/
 
-// paramétrage du port avec la méthode set() de Express
+// Paramétrage du port avec la méthode set() de Express et utiliser process.env qui appelle la clé PORT qui retourne sa valeur définie dans votre fichier .env.
 
-app.set("port", 3000);
+app.set("port", process.env.PORT);
+
+const result = dotenv.config();
+console.log(result);
 
 // Méthode createServer() qui pour chaque requête appelle les fonctions du fichier app.js
 
@@ -18,4 +25,4 @@ const server = http.createServer(app);
 
 // Écoute des requêtes par le serveur sur le port 3000
 
-server.listen(3000);
+server.listen(process.env.PORT);
