7ce384ebb60a56bd2028499f94d16ffb221fbb36
Christophe Romand <ackheron@tuta.io>
2022-01-07 13:19:07 +0100

    Ajout du module morgan (http request logger)

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+// Après l'installation du framework Express avec la ligne de commande "npm install express" on l'importe
+
+const express = require("express");
+
+// Creation de l'application app avec la méthode express()
+
+const app = express();
+
+/*********************MODULES COMPLÉMENTAIRES **********************/
+
+// importation de morgan "http request logger"
+const morgan = require("morgan");
+
+/*********************** CORS ****************************/
+
+// app.use
+
+// log des request et des response
+app.use(morgan("dev"));
+
+// route générale
+app.use((req, res, next) => {
+  console.log("requête numéro 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "requête numéro 1 et 2 et 3 4" });
+});
+
+// exportation de app.js pour que les autres fichiers puissent y accéder
+
+module.exports = app;
