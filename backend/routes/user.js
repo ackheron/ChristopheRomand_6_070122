ab0e172d0c2840c682e76b109b80d8c3c6b87e6f
Ackheron <ackheron@etik.com>
2022-01-14 14:25:20 +0100

    Routes sauces.js et user.js

---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+//importations
+const express = require("express");
+const router = express.Router();
+// const userController = require("../controllers/user");
+const password = require("../middleware/password");
+
+//les routes
+router.post("/signup", password, userController.signup);
+router.post("/login", userController.login);
+
+//exportation
+module.exports = router;
