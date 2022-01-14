7789772683dc9de6b600fad24cd214caeecfff1b
Christophe Romand <ackheron@tuta.io>
2022-01-14 14:25:20 +0100

    Routes sauces.js et user.js

---
+++ backend/routes/sauces.js
@@ -0,0 +1,25 @@
+//importation
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+// const sauceControllers = require("../controllers/sauce");
+// const likeControllers = require("../controllers/like")
+
+//Route POST pour créer une sauce
+router.post("/", auth, multer, sauceControllers.createSauce);
+
+router.put("/:id", auth, multer, sauceControllers.modifySauce);
+
+router.delete("/:id", auth, sauceControllers.deleteSauce);
+
+router.get("/", auth, sauceControllers.getAllSauce);
+
+router.get("/:id", auth, sauceControllers.getOneSauce);
+
+router.post("/:id/like", auth, likeControllers.likeSauce);
+
+//exportation
+module.exports = router;
